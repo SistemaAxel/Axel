c14b19f4e03a70deb4b5e4d77a866427bc2398d2
109038805+naielv@users.noreply.github.com <109038805+naielv@users.noreply.github.com>
2023-11-25 22:10:52 +0100

    update main.py

--- main.py
+++ main.py
@@ -19,12 +19,12 @@ def get_menu_today(menu):
     fecha = menu[menu["Fecha"] == get_date()]
     if len(fecha["Fecha"].iloc[:1]) > 0:
       #e = {
-        "Fecha": fecha["Fecha"].iloc[:1][0],
-        "Plato1": fecha["Plato1"].iloc[:1][0],
-        "Plato2": fecha["Plato2"].iloc[:1][0],
-        "Postre": fecha["Postre"].iloc[:1][0],
-        "Pan": fecha["Pan"].iloc[:1][0],
-      }
+      #  "Fecha": fecha["Fecha"].iloc[:1][0],
+      #  "Plato1": fecha["Plato1"].iloc[:1][0],
+      #  "Plato2": fecha["Plato2"].iloc[:1][0],
+      #  "Postre": fecha["Postre"].iloc[:1][0],
+      #  "Pan": fecha["Pan"].iloc[:1][0],
+      #}
       e = None
       print(fecha["Fecha"].iloc[:1])
     else:
